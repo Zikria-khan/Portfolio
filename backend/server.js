279a11cb93457c65b4f73e39038760b93816a3ca
Practice <coding4443@gmail.com>
2024-11-04 12:45:41 -0800

    some

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  }));
+  
 app.use(bodyParser.json());
 
 // MongoDB connection
